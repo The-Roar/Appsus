7c506408905b9a718e5d4afc097a5914ff26570b
GalGolikov <GalGolikovv@gmail.com>
2021-05-06 16:32:02 +0300

    handle youtube short and long url

--- missKeep/cmps/noteTypes/NoteVideo.jsx
+++ missKeep/cmps/noteTypes/NoteVideo.jsx
@@ -1,17 +1,39 @@
 export function NoteVideo({ note }) {
-  const embedId = note.content.video.split('https://youtu.be/')[1];
+  const videoUrl = note.content.video;
+  let urlType = videoUrl.includes('https://www.youtube.com/watch?v=')
+    ? 'long'
+    : videoUrl.includes('https://youtu.be/')
+    ? 'short'
+    : false;
+
+  let embedId = '';
+  switch (urlType) {
+    case 'short':
+      embedId = videoUrl.split('https://youtu.be/')[1];
+      break;
+    case 'long':
+      embedId = videoUrl.split('https://www.youtube.com/watch?v=')[1];
+      break;
+    default:
+      embedId = videoUrl.split('https://www.youtube.com/watch?v=')[1];
+  }
+
   return (
     <div className='video-note'>
       <h3>{note.content.title}</h3>
-      <iframe
-        width='853'
-        height='480'
-        src={`https://www.youtube.com/embed/${embedId}?controls=0`}
-        frameBorder='0'
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        allowFullScreen
-        title='Embedded youtube'
-      />
+      {urlType ? (
+        <iframe
+          width='853'
+          height='480'
+          src={`https://www.youtube.com/embed/${embedId}?controls=0`}
+          frameBorder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+          title='Embedded youtube'
+        />
+      ) : (
+        <p>Couldn't load video</p>
+      )}
     </div>
   );
 }
